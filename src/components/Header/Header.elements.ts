3a373a396543b89d4841be85c9ab8b6a639c4ba7
TiagoDiass <tiago.costadiasss@gmail.com>
2021-01-22 23:48:10 -0300

    chore: adding header component

--- src/components/Header/Header.elements.ts
+++ src/components/Header/Header.elements.ts
@@ -1,3 +1,35 @@
 import styled from 'styled-components';
+import { FaGithub } from 'react-icons/fa';
 
-export const Container = styled.div``;
+export const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  background: var(--header);
+  padding: 11px 16px;
+`;
+
+export const GithubLogo = styled(FaGithub)`
+  fill: var(--logo);
+  width: 32px;
+  height: 32px;
+  flex-shrink: 0;
+`;
+
+export const SearchForm = styled.form`
+  padding-left: 16px;
+  width: 100%;
+
+  input {
+    background: var(--search);
+    outline: 0;
+    border-radius: 6px;
+    padding: 7px 12px;
+    width: 100%;
+    transition: width 0.2s ease-out, color 0.2s ease-out;
+
+    &:focus {
+      width: 318px;
+    }
+  }
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, GithubLogo, SearchForm } from './Header.elements';
 
 const Header: React.FC = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <GithubLogo />
+      <SearchForm>
+        <input type='text' placeholder='Enter Username or Repo...' />
+      </SearchForm>
+    </Container>
+  );
 };
 
 export default Header;
