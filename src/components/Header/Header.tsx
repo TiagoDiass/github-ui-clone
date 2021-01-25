30378b74bbfc89a598dbb2639cb07a295ddbae5e
Tiago Dias <tiago.costadiasss@gmail.com>
2021-01-25 13:15:50 -0300

    feat: adding logic to fetch through the header search bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Container, GithubLogo, SearchForm } from './Header.elements';
 
 const Header: React.FC = () => {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    navigate(`/${search.toLocaleLowerCase().trim()}`);
+  }
+
   return (
     <Container>
       <GithubLogo />
-      <SearchForm>
-        <input type='text' placeholder='Enter Username or Repo...' />
+      <SearchForm onSubmit={handleSubmit}>
+        <input
+          value={search}
+          onChange={e => setSearch(e.currentTarget.value)}
+          type='text'
+          placeholder='Enter Username or Repo...'
+        />
       </SearchForm>
     </Container>
   );
